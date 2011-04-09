c9f5c413896be752d99e0980f56f9ce75ca5e3b1
Jonathan Jaubart <dev@jaubart.com>
2011-04-09 15:45:01 +0000

    RESOLVED - # 63: Implement Hooks architecture http://bugs.jaubart.com/view.php?id=63
    git-svn-id: http://subversion.assembla.com/svn/webtrees-geneajaubart/trunk@8 2443a68c-d498-3b47-9643-c9de34c2b0bc

---
+++ library/WT/Perso/Hook.php
@@ -0,0 +1,214 @@
+<?php
+/**
+ * Class to manage Hooks (subscription and execution
+ *
+ * @package webtrees
+ * @subpackage PersoLibrary
+ * @author: Jonathan Jaubart ($Author$)
+ * @version: p_$Revision$ $Date$
+ * $HeadURL$
+ */
+
+if (!defined('WT_WEBTREES')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+class WT_Perso_Hook {
+
+	//Private variables
+	protected $hook_function; //Hook function
+
+	//Constants
+	private static $DEFAULT_PRIORITY = 99;
+
+	/**
+	 * Constructor for Hook class
+	 *
+	 * @param string $hook_function_in Hook function to be subscribed or executed
+	 */
+	public function __construct($hook_function_in){
+		$this->hook_function = $hook_function_in;
+	}
+
+	/**
+	 * Methods for subscribing to Hooks
+	 */
+
+
+	/**
+	 * Subscribe a class implementing WT_Perso_Module_HookSubscriber to the Hook
+	 * The Hook is by default enabled.
+	 *
+	 * @param string $hsubscriber Name of the subscriber module
+	 */
+	public function subscribe($hsubscriber){
+		$statement = WT_DB::prepare(
+			"INSERT IGNORE INTO `##phooks` (ph_hook_function, ph_module_name)".
+			" VALUES (?, ?)"
+			)->execute(array($this->hook_function, $hsubscriber));
+	}
+
+	/**
+	 *  Define the priority for execution of the Hook for the specific WT_Perso_Module_HookSubscriber
+	 *
+	 * @param string $hsubscriber Name of the subscriber module
+	 * @param int $priority Priority of execution
+	 */
+	public function setPriority($hsubscriber, $priority){
+		WT_DB::prepare(
+			"UPDATE `##phooks`".
+			" SET ph_module_priority=?".
+			" WHERE ph_hook_function=?".
+			" AND ph_module_name=?"
+			)->execute(array($priority, $this->hook_function, $hsubscriber));
+	}
+
+	/**
+	 * Enable the hook for a specific WT_Perso_Module_HookSubscriber.
+	 *
+	 * @param string $hsubscriber Name of the subscriber module
+	 */
+	public function enable($hsubscriber){
+		WT_DB::prepare(
+			"UPDATE `##phooks`".
+			" SET ph_status='enabled'".
+			" WHERE ph_hook_function=?".
+			" AND ph_module_name=?"
+			)->execute(array($this->hook_function, $hsubscriber));
+	}
+
+	/**
+	 * Disable the hook for a specific WT_Perso_Module_HookSubscriber.
+	 *
+	 * @param string $hsubscriber Name of the subscriber module
+	 */
+	public function disable($hsubscriber){
+		WT_DB::prepare(
+			"UPDATE `##phooks`".
+			" SET ph_status='disabled'".
+			" WHERE ph_hook_function=?".
+			" AND ph_module_name=?"
+			)->execute(array($this->hook_function, $hsubscriber));
+	}
+
+	/**
+	 * Remove the hook for a specific WT_Perso_Module_HookSubscriber.
+	 *
+	 * @param string $hsubscriber Name of the subscriber module
+	 */
+	public function remove($hsubscriber){
+		WT_DB::prepare(
+			"DELETE FROM `##phooks`".
+			" WHERE ph_hook_function=?".
+			" AND ph_module_name=?"
+			)->execute(array($this->hook_function, $hsubscriber));
+	}
+
+
+	/**
+	 * Methods for execution of the Hook
+	 *
+	 */
+
+	/**
+	 * Execute the hook function for all subscribed and enabled modules, in the order defined by their priority.
+	 * Parameters can be passed if the hook requires them.
+	 *
+	 */
+	public function execute(){
+		$params = func_get_args();
+		$module_names=WT_DB::prepare(
+			"SELECT ph_module_name AS module, ph_module_priority AS priority FROM `##phooks`".
+			" WHERE ph_hook_function = ? AND ph_status='enabled'".
+			" ORDER BY ph_module_priority ASC, module ASC"
+			)->execute(array($this->hook_function))->fetchAssoc();
+			asort($module_names);
+			foreach ($module_names as $module_name => $module_priority) {
+				require_once WT_ROOT.WT_MODULES_DIR.$module_name.'/module.php';
+				$class=$module_name.'_WT_Module';
+				$hook_class=new $class();
+				call_user_func_array(array($hook_class, $this->hook_function), $params);
+			}
+	}
+
+	/**
+	 * Static functions for Hooks.
+	 */
+
+
+	/**
+	 * Get the list of possible hooks in the list of modules files.
+	 * A hook will be registered:
+	 * 		- for all modules already registered in Webtrees
+	 * 		- if the module implements WT_Perso_Module_HookSubscriber nterface
+	 * 		- if the method exist within the module
+	 *
+	 * @return Array List of possible hooks, with the priority
+	 */
+	static public function getPossibleHooks() {
+		static $hooks=null;
+		if ($hooks===null) {
+			$modules = $module_names=WT_DB::prepare("SELECT module_name FROM `##module`")->execute()->fetchOneColumn();
+			$dir=opendir(WT_ROOT.WT_MODULES_DIR);
+			while (($file=readdir($dir))!==false) {
+				if (preg_match('/^[a-zA-Z0-9_]+$/', $file) && file_exists(WT_ROOT.WT_MODULES_DIR.$file.'/module.php')) {
+					require_once WT_ROOT.WT_MODULES_DIR.$file.'/module.php';
+					$class=$file.'_WT_Module';
+					$hook_class=new $class();
+					if( in_array($file, $modules) && $hook_class instanceof WT_Perso_Module_HookSubscriber){
+						$subscribedhooks = $hook_class->getSubscribedHooks();
+						if(is_array($subscribedhooks)){
+							foreach($subscribedhooks as $key => $value){
+								if(is_int($key)) {
+									$hook_item = $value;
+									$priority = WT_Perso_Hook::$DEFAULT_PRIORITY;
+								}
+								else{
+									$hook_item = $key;
+									$priority = $value;
+								}
+								if(method_exists($hook_class, $hook_item)){
+									$hooks[$hook_class->getName().'#'.$hook_item]=$priority;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return $hooks;
+	}
+	
+	/**
+	 * Get the list of hooks intalled in webtrees, with their id, status and priority.
+	 *
+	 * @return DBOStatement List of installed hooks
+	 */
+	static public function getRawInstalledHooks(){
+		return WT_DB::prepare(
+			"SELECT ph_id AS id, ph_module_name AS module, ph_hook_function AS hook, ph_module_priority AS priority,  ph_status AS status".
+			" FROM `##phooks`".
+			" ORDER BY hook ASC, status ASC, priority ASC, module ASC"
+		)->execute()->fetchAll();
+	}
+
+	/**
+	 * Get the list of hooks intalled in webtrees, with their id, status and priority.
+	 *
+	 * @return Array List of installed hooks, with id, status and priority
+	 */
+	static public function getInstalledHooks(){
+		static $installedhooks =null;
+		if($installedhooks===null){
+			$dbhooks=WT_Perso_Hook::getRawInstalledHooks();
+			foreach($dbhooks as $dbhook){
+				$installedhooks[($dbhook->module).'#'.($dbhook->hook)] = array('id' => $dbhook->id, 'status' => $dbhook->status, 'priority' => $dbhook->priority);
+			}
+		}
+		return $installedhooks;
+	}
+
+}
+
+?>
---
+++ modules_v2/perso_hooks/module.php
@@ -0,0 +1,237 @@
+<?php
+/**
+ * Classes for Module Perso Hooks
+ *
+ * @package webtrees
+ * @subpackage Perso
+ * @author: Jonathan Jaubart ($Author$)
+ * @version: p_$Revision$ $Date$
+ * $HeadURL$
+ */
+
+if (!defined('WT_WEBTREES')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+// Create tables, if not already present
+try {
+	WT_DB::updateSchema(WT_ROOT.WT_MODULES_DIR.'perso_hooks/db_schema/', 'PHOOKS_SCHEMA_VERSION', 1);
+} catch (PDOException $ex) {
+	// The schema update scripts should never fail.  If they do, there is no clean recovery.
+	die($ex);
+}
+
+class perso_hooks_WT_Module extends WT_Module implements WT_Module_Config {
+	
+	// Extend class WT_Module
+	public function getTitle() {
+		return WT_I18N::translate('Hooks (Perso)');
+	}
+	
+	// Extend class WT_Module
+	public function getDescription() {
+		return WT_I18N::translate('Implements hooks management.');
+	}
+	
+	// Extend WT_Module
+	public function modAction($mod_action) {
+		switch($mod_action) {
+		case 'admin_config':
+			$this->updateHooks();
+			$this->updateParams();
+			$this->config();
+			break;
+		default:
+			header('HTTP/1.0 404 Not Found');
+		}
+	}
+	
+	// Implement WT_Module_Config
+	public function getConfigLink() {
+		return 'module.php?mod='.$this->getName().'&mod_action=admin_config';
+	}
+
+	/**
+	 * Compare possible and installed hooks and subscribe/remove if required.
+	 */
+	private function updateHooks(){
+		
+		if(WT_USER_IS_ADMIN){
+			$ihooks = WT_Perso_Hook::getInstalledHooks();
+			$phooks = WT_Perso_Hook::getPossibleHooks();
+			
+			// Insert hooks not existing yet in the DB
+			if($phooks!=null){
+				foreach($phooks as $phook => $priority){
+					$array_hook = explode('#', $phook);
+					if($ihooks==null || !array_key_exists($phook, $ihooks)){
+						$chook = new WT_Perso_Hook($array_hook[1]);
+						$chook->subscribe($array_hook[0]);
+						$chook->setPriority($array_hook[0], $priority);
+					}
+				}
+			}
+			
+			//Remove hooks not existing any more in the file system
+			if($ihooks!=null){
+				foreach($ihooks as $ihook => $status){
+					$array_hook = explode('#', $ihook);
+					if($phooks==null || !array_key_exists($ihook, $phooks)){
+						$chook = new WT_Perso_Hook($array_hook[1]);
+						$chook->remove($array_hook[0]);
+					}
+				}
+			}
+		}
+		
+	}
+	
+	/**
+	 * Update the status and the priority based on user choices or changes in underlying modules.
+	 * By default, disabled modules involves disabled hooks.
+	 * 
+	 */
+	private function updateParams(){
+		
+		if(WT_USER_IS_ADMIN){
+			$ihooks = WT_Perso_Hook::getInstalledHooks();
+			
+			$module_names=WT_DB::prepare("SELECT module_name FROM `##module` WHERE status='disabled'")->fetchOneColumn();		
+			
+			if($ihooks!=null){
+				foreach ($ihooks as $ihook => $params) {
+					$array_hook = explode('#', $ihook);
+					//Update status
+					$new_status=safe_POST("status-{$params['id']}");
+					if(in_array($array_hook[0], $module_names)) $new_status=0;
+					$previous_status=$params['status'];	
+					if ($new_status!==null) {
+						$new_status= $new_status ? 'enabled' : 'disabled';
+						if($new_status != $previous_status){
+							$chook = new WT_Perso_Hook($array_hook[1]);
+							switch($new_status){
+								case 'enabled':
+									$chook->enable($array_hook[0]);
+									break;
+								case 'disabled':
+									$chook->disable($array_hook[0]);
+									break;
+								default:
+									break;
+							}
+						}
+					}
+					//Update priority
+					$new_priority=safe_POST("moduleorder-{$params['id']}");
+					$previous_priority=$params['priority'];	
+					if ($new_priority!==null) {
+						if($new_priority != $previous_priority){
+							$chook = new WT_Perso_Hook($array_hook[1]);
+							$chook->setPriority($array_hook[0], $new_priority);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	
+	/**
+	 * Display an editable list of installed hooks in order for the admin to configure statuses and priorities.
+	 */
+	private function config() {
+
+		if(WT_USER_IS_ADMIN) {
+		
+			require WT_ROOT.'includes/functions/functions_edit.php';
+			
+			print_header($this->getTitle());
+	
+			$hooks= WT_Perso_Hook::getRawInstalledHooks();
+	
+			?>
+			<script type="text/javascript">
+			//<![CDATA[
+			
+			  function reindexMods(id) {
+					jQuery('#'+id+' input').each(
+						function (index, value) {
+							value.value = index+1;
+						});
+			  }
+			
+			  jQuery(document).ready(function() {
+	
+				  var oTable = jQuery('#installed_table').dataTable( {
+						"oLanguage": {
+						"sLengthMenu": '<?php echo /* I18N: %s is a placeholder for listbox containing numeric options */ WT_I18N::translate('Display %s', '<select><option value="10">10</option><option value="20">20</option><option value="30">30</option><option value="40">40</option><option value="50">50</option><option value="-1">'.WT_I18N::translate('All').'</option></select>'); ?>',
+						"sZeroRecords": '<?php echo WT_I18N::translate('No records to display');?>',
+						"sInfo": '<?php echo /* I18N: %s are placeholders for numbers */ WT_I18N::translate('Showing %1$s to %2$s of %3$s', '_START_', '_END_', '_TOTAL_'); ?>',
+						"sInfoEmpty": '<?php echo /* I18N: %s are placeholders for numbers */ WT_I18N::translate('Showing %1$s to %2$s of %3$s', '0', '0', '0'); ?>',
+						"sInfoFiltered": '<?php echo /* I18N: %s is a placeholder for a number */ WT_I18N::translate('(filtered from %s total entries)', '_MAX_'); ?>',
+						"sSearch": '<?php echo WT_I18N::translate('Search');?>',
+						"oPaginate": {
+							"sFirst": '<?php echo /* I18N: button label, first page    */ WT_I18N::translate('first'); ?>',
+							"sLast": '<?php echo /* I18N: button label, last page     */ WT_I18N::translate('last'); ?>',
+							"sNext": '<?php echo /* I18N: button label, next page     */ WT_I18N::translate('next'); ?>',
+							"sPrevious": '<?php echo /* I18N: button label, previous page */ WT_I18N::translate('previous'); ?>'
+						}
+						},
+						"sDom": '<"H"prf>t<"F"li>',
+						"bJQueryUI": true,
+						"bAutoWidth":false,
+						"aaSorting": [[ 1, "asc" ]],
+						"iDisplayLength": 10,
+						"sPaginationType": "full_numbers",
+						"aoColumnDefs": [
+							{ "bSortable": false, "aTargets": [ 0, 3 ] }
+						]
+				  });
+	
+			});
+			//]]>
+			</script>
+			
+			<?php
+			
+			echo '<div align="center">',
+				'<div id="tabs">';
+			echo WT_I18N::translate('Help').help_link('admin_config', $this->getName());
+			echo '<form method="post" action="#">',
+						'<table id="installed_table" class="tablesorter" border="0" cellpadding="0" cellspacing="1">',
+							'<thead>',
+								'<tr>',
+								'<th>',WT_I18N::translate('Enabled'),'</th>',
+								'<th>',WT_I18N::translate('Hook Function'),'</th>',
+								'<th>',WT_I18N::translate('Module Name'),'</th>',
+								'<th>',WT_I18N::translate('Priority (1 is high)'),'</th>',
+								'</tr>',
+							'</thead>',
+							'<tbody>';
+								foreach ($hooks as $id => $hook) {
+									echo '<tr><td>', two_state_checkbox('status-'.($hook->id), ($hook->status)=='enabled'), '</td>',
+										'<td>',$hook->hook,'</td>',
+										'<td>',$hook->module,'</td>',
+										'<td><input type="text" class="center" size="2" value="',$hook->priority,'" name="moduleorder-',$hook->id,'" /></td>',
+										'</tr>';
+								}
+			echo 			'</tbody>',
+						'</table>',
+						'<input type="submit" value="',WT_I18N::translate('Save'),'" />',
+					'</form>',
+				'</div>',
+			'</div>';
+	
+			print_footer();
+		
+		} else {
+			header('Location: '.WT_SERVER_NAME.WT_SCRIPT_PATH);
+			exit;
+		}
+	}
+
+}
+
+
+?>
