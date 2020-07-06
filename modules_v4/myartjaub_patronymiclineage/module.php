2edded14b56c0d990dfea0f886c8e0e592379868
Jonathan Jaubart <dev@jaubart.com>
2020-07-06 23:58:49 +0100

    Upgrade to webtrees 2.0.6 - Adapt MyArtJaub Patronymic Lineages Tasks module

---
+++ modules_v4/myartjaub_patronymiclineage/module.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * MyArtJaub Patronymic Lineages module
+ *
+ * @package MyArtJaub\Webtrees
+ * @subpackage PatronymicLineage
+ * @author Jonathan Jaubart <dev@jaubart.com>
+ * @copyright Copyright (c) 2009-2020, Jonathan Jaubart
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3
+ */
+declare(strict_types=1);
+
+namespace MyArtJaub\Webtrees\Module\PatronymicLineage;
+
+return app(PatronymicLineageModule::class);
+
+?>
---
+++ modules_v4/myartjaub_patronymiclineage/resources/lang/fr/messages.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * MyArtJaub Patronymic Lineages module
+ *
+ * @package MyArtJaub\Webtrees
+ * @subpackage PatronymicLineage
+ * @author Jonathan Jaubart <dev@jaubart.com>
+ * @copyright Copyright (c) 2009-2020, Jonathan Jaubart
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3
+ */
+declare(strict_types=1);
+
+use Fisharebest\Localization\Translation;
+
+return array (
+
+    'Patronymic Lineages'           =>  'Lignées Patronymiques',
+    'Display lineages of people holding the same surname.'
+                                    =>  'Affiche les lignées des individus portant le même nom de famille.',
+    'Individuals in %s lineages'    =>  'Individus des lignées %s',
+    'All lineages'                  =>  'Toutes les lignées',
+    'Go to the list of individuals with surname %s'
+                                    =>  'Voir la liste des individus portant le nom %s',
+    'No individuals with surname %s has been found. Please try another name.'
+                                    =>  'Aucun individu portant le nom %s n\'a été trouvé. Veuillez essayer un autre patronyme.',
+    '%s lineage found' . Translation::PLURAL_SEPARATOR . '%s lineages found'
+                                    =>  '%s lignée trouvée'. Translation::PLURAL_SEPARATOR . '%s lignées trouvées',
+    'Go to %s lineages'             =>  'Voir les lignées %s',
+    'Informations for individual %s'=>  'Informations sur l\'individu %s',
+    'Unknown surname'               =>  'Patronyme inconnu'
+        
+);
