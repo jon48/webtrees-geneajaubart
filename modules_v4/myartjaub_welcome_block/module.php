f5dcbe4e084b879a0b5b6856729311c70ca56665
Jonathan Jaubart <dev@jaubart.com>
2020-07-14 22:20:34 +0100

    Upgrade to webtrees 2.0.6 - Adapt MyArtJaub WelcomeBlock module

---
+++ modules_v4/myartjaub_welcome_block/module.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * webtrees-lib: MyArtJaub library for webtrees
+ *
+ * @package MyArtJaub\Webtrees
+ * @subpackage WelcomeBlock
+ * @author Jonathan Jaubart <dev@jaubart.com>
+ * @copyright Copyright (c) 2011-2020, Jonathan Jaubart
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3
+ */
+
+declare(strict_types=1);
+
+namespace MyArtJaub\Webtrees\Module\WelcomeBlock;
+
+return app(WelcomeBlockModule::class);
---
+++ modules_v4/myartjaub_welcome_block/resources/lang/fr/messages.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * webtrees-lib: MyArtJaub library for webtrees
+ *
+ * @package MyArtJaub\Webtrees
+ * @subpackage WelcomeBlock
+ * @author Jonathan Jaubart <dev@jaubart.com>
+ * @copyright Copyright (c) 2011-2020, Jonathan Jaubart
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3
+ */
+
+declare(strict_types=1);
+
+return array (
+    
+    'MyArtJaub Welcome Block'       =>  'Bloc «Accueil MyArtJaub»',
+    'The MyArtJaub Welcome block welcomes the visitor to the site, allows a quick login to the site, and displays statistics on visits.'
+                                    =>  'Le bloc Accueil MyArtJaub acueille le visiteur sur le site, permet une connexion rapide et affiche des statistiques de visites.',
+    'Enable Matomo Statistics'      =>  'Activer les statistiques Matomo',
+    'Matomo URL'                    =>  'Adresse URL de Matomo',
+    'Matomo Token'                  =>  'Token Matomo',
+    'Matomo Site ID'                =>  'ID Matomo du site',
+    'Retrieving Matomo statistics...'
+                                    =>  'Récupération des statistiques Matomo...',
+    'No statistics could be retrieved from Matomo.'
+                                    =>  'Les statistiques Matomo ne peuvent être récupérées.',
+    '%1$s visits since the beginning of %2$s<br>(%3$s today)'
+                                    =>  '%1$s visiteurs depuis le début de l\'année %2$s<br>dont %3$s aujourd\'hui',
+    'Continue'                      =>  'Continuer',
+    'The Matomo URL provided is not valid.'
+                                    =>  'L\'adresse URL Matomo renseignée n\'est pas valide.',
+    'The Matomo Site ID provided is not valid.'
+                                    =>  'L\'ID Matomo de site renseigné n\'est pas valide.'
+    
+);
