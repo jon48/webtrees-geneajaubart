fb53141a95723881283dead51be5fd09c42eca12
Jonathan Jaubart <dev@jaubart.com>
2021-03-20 15:26:06 +0000

    Upgrade to webtrees 2.1.0 - Adapt MyArtJaub Certificates module (Partial - Config, List, Display with watermark)

---
+++ modules_v4/myartjaub_certificates/module.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * MyArtJaub Certificates module
+ *
+ * @package MyArtJaub\Webtrees
+ * @subpackage Certificates
+ * @author Jonathan Jaubart <dev@jaubart.com>
+ * @copyright Copyright (c) 2009-2021, Jonathan Jaubart
+ * @license https://www.gnu.org/licenses/gpl.html GNU General Public License, version 3
+ */
+
+declare(strict_types=1);
+
+namespace MyArtJaub\Webtrees\Module\Certificates;
+
+return app(CertificatesModule::class);
