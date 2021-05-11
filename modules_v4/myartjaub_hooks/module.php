89495e68562c56b515d2746bf2f64d3cc756d3c2
Jonathan Jaubart <dev@jaubart.com>
2021-05-11 11:21:26 +0100

    Upgrade to webtrees 2.1.0 - Adapt MyArtJaub Hooks module

---
+++ modules_v4/myartjaub_hooks/module.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *  MyArtJaub Hooks module
+ *
+ * @package MyArtJaub\Webtrees
+ * @subpackage Hooks
+ * @author Jonathan Jaubart <dev@jaubart.com>
+ * @copyright Copyright (c) 2011-2021, Jonathan Jaubart
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3
+ */
+
+declare(strict_types=1);
+
+namespace MyArtJaub\Webtrees\Module\Hooks;
+
+return app(HooksModule::class);
---
+++ modules_v4/myartjaub_hooks/resources/lang/fr/messages.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ *  MyArtJaub Hooks module
+ *
+ * @package MyArtJaub\Webtrees
+ * @subpackage Hooks
+ * @author Jonathan Jaubart <dev@jaubart.com>
+ * @copyright Copyright (c) 2011-2021, Jonathan Jaubart
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3
+ */
+
+declare(strict_types=1);
+
+return array (
+
+    'Hooks'                         =>  'Hooks',
+    'Implements hooks management.'  =>  'Implémente la gestion des Hooks',
+    'Select a hook to configure the loading order of the modules implementing it.'
+                                    =>  'Choisir un Hook pour configurer l’ordre de chargement des modules l’implémentant.',
+    'The hook preferences have been updated.'
+                                    =>  'Les préférences du Hook ont été mises à jour.',
+    'The hook with name “%s” does not exist.'
+                                    =>  'Le Hook avec le nom « %s » n’existe pas.',
+    'Custom simple tag editor'      =>  'Éditeur de balises personnalisées simples',
+    'Allows for the edition of simple custom tags.'
+                                    =>  'Permet l’édition des balises personnalisées simples',
+    'Text extender for records’ name'
+                                    =>  'Extension du nom des enregistrements',
+    'Extends the full name of GEDCOM records with additional text or icons.'
+                                    =>  'Étend le nom complet des enregistrement GEDCOM avec du texte ou des icônes supplémentaires.',
+    'Text extender for source citations’ title'
+                                    =>  'Extension du titre des citations',
+    'Extends the title of source citations with additional text or icons.'
+                                    =>  'Étend le titre des citations avec du texte ou des icônes supplémentaires.',
+
+);
