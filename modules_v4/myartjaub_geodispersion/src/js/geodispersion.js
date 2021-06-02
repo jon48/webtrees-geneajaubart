420bbe38752c9a8340c6edcd2b8c98a361251956
Jonathan Jaubart <dev@jaubart.com>
2021-06-02 11:47:32 +0100

    Upgrade to webtrees 2.1.0 - Adapt MyArtJaub modules Certificates: Fix Cannot use colon in element ID with jQuery Hooks: Remove CustomSimpleTagEditorInterface hook Hooks: Move NameAccordionExtenderInterface hook to refactored view GeoDispersion: Move JS code from view to module JS library GeoDispersion: Implement multiple map providers Sosa: Fix deprecation of getRelationshipNameFromPath method Sosa: Fix ModuleSidebarInterface::sidebarTitle new signature with Individual record

--- modules_v4/myartjaub_geodispersion/src/js/geodispersion.js
+++ modules_v4/myartjaub_geodispersion/src/js/geodispersion.js
@@ -21,6 +21,9 @@
  * along with webtrees-MyArtJaub. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* global L */
+
+// Add required icons to Font-Awesome
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 import {
@@ -28,3 +31,190 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faMapMarkedAlt, faTable);
+
+// MyArtJaub GeoDispersion library
+(function (majGeodispersion) {
+  /** API object */
+
+  /**
+   * Feature Style object
+   *
+   * @param {{default: Object, stroke: Object, lowColor: Object, maxColor: Object, hover: Object}} colorConfig
+   */
+  majGeodispersion.MapFeaturesStyle = function (colorConfig) {
+    /**
+     * Returns features default style
+     *
+     * @returns {Object}
+     */
+    function defaultStyle () {
+      return {
+        fillColor: colorString(colorConfig.defaultColor),
+        weight: 1,
+        opacity: 1,
+        color: colorString(colorConfig.strokeColor),
+        fillOpacity: 1
+      };
+    }
+
+    /**
+     * Returns features hover style
+     *
+     * @returns {Object}
+     */
+    function hoverStyle () {
+      return { ...defaultStyle(), fillColor: colorString(colorConfig.hoverColor) };
+    }
+
+    /**
+     * Returns style for a feature based on its data
+     *
+     * @returns {Object}
+     */
+    function featureStyle (feature) {
+      return feature.properties.color_ratio !== undefined
+        ? {
+            ...defaultStyle(),
+            fillColor: colorString(
+              intermediateColor(
+                feature.properties.color_ratio,
+                colorConfig.lowColor,
+                colorConfig.highColor
+              )
+            )
+          }
+        : defaultStyle();
+    }
+
+    return {
+      colorConfig: colorConfig,
+      defaultStyle: defaultStyle,
+      hoverStyle: hoverStyle,
+      featureStyle: featureStyle
+    };
+  };
+
+  /** API Functions */
+
+  /**
+   * Draws a Leaflet map at a specified element
+   *
+   * @param {string} elementId
+   * @param {{i18n: Object, icons: Object, mapProviders: Object}} config
+   * @returns {MapObject}
+   */
+  majGeodispersion.drawMap = function (elementId, config) {
+    const dataLayer = new L.FeatureGroup();
+
+    let defaultLayer = null;
+
+    for (const [, provider] of Object.entries(config.mapProviders)) {
+      for (const [, child] of Object.entries(provider.children)) {
+        if ('bingMapsKey' in child) {
+          child.layer = L.tileLayer.bing(child);
+        } else {
+          child.layer = L.tileLayer(child.url, child);
+        }
+        if (provider.default && child.default) {
+          defaultLayer = child.layer;
+        }
+      }
+    }
+
+    if (defaultLayer === null) {
+      console.log('No default map layer defined - using the first one.');
+      defaultLayer = config.mapProviders[0].children[0].layer;
+    }
+
+    const map = L.map(elementId, {
+      center: [0, 0],
+      zoomSnap: 0,
+      zoomControl: false, // disable zoom
+      scrollWheelZoom: false // disable zoom
+    })
+      .addLayer(defaultLayer)
+      .addLayer(dataLayer)
+      .addControl(L.control.layers.tree(config.mapProviders, null, {
+        closedSymbol: config.icons.expand,
+        openedSymbol: config.icons.collapse
+      }));
+
+    return new MapObject(map, dataLayer);
+  };
+
+  /**
+   * Adds a GeoJson data layer to the map
+   *
+   * @param {MapObject} map
+   * @param {Object} data
+   * @param {MapFeaturesStyle} style
+   */
+  majGeodispersion.addGeoJsonDataToMap = function (map, data, style) {
+    if (map.dataLayer === undefined || map.leafletMap === undefined) return;
+
+    const geoJsonLayer = L.geoJson(data, {
+      onEachFeature: function (feature, layer) {
+        if (feature.properties.tooltip !== undefined) {
+          layer.bindTooltip(feature.properties.tooltip);
+
+          // Set the highlight style into layer when 'mouseover'
+          (function () {
+            layer.on('mouseover', function () {
+              // Set the style with classified color
+              layer.setStyle(style.hoverStyle());
+            });
+            layer.on('mouseout', function () {
+              layer.setStyle(style.featureStyle(feature));
+            });
+          })(layer, feature);
+        }
+      },
+      style: style.featureStyle
+    });
+
+    if (data.features.length > 0) {
+      map.dataLayer.addLayer(geoJsonLayer);
+    }
+    map.leafletMap.fitBounds(map.dataLayer.getBounds(), { padding: [50, 50] });
+  };
+
+  /** Private Objects */
+
+  /**
+   * @private
+   * @param {Map} map
+   * @param {FeatureGroup} dataLayer
+   * @returns {{leafletMap: Map, dataLayer: FeatureGroup}}
+   */
+  function MapObject (map, dataLayer) {
+    this.leafletMap = map;
+    this.dataLayer = dataLayer;
+  }
+
+  /** Private Functions */
+  /**
+   * @private
+   * @param {{r: number, g: number, b; number}} color
+   * @returns {string}
+   */
+  function colorString (color) {
+    return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
+  }
+
+  /**
+   * @private
+   * @param {number} pct
+   * @param {{r: number, g: number, b; number}} lowColor
+   * @param {{r: number, g: number, b; number}} highColor
+   * @returns {{r: number, g: number, b; number}}
+   */
+  function intermediateColor (pct, lowColor, highColor) {
+    const pctLower = 1 - pct;
+    const pctUpper = pct;
+    return {
+      r: Math.floor(lowColor.r * pctLower + highColor.r * pctUpper),
+      g: Math.floor(lowColor.g * pctLower + highColor.g * pctUpper),
+      b: Math.floor(lowColor.b * pctLower + highColor.b * pctUpper)
+    };
+  }
+}(window.majGeodispersion = window.majGeodispersion || {}));
